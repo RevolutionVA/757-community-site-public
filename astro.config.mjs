92d2a506123ac7d4bc7ac53b8dadbec7d15e7c91
Kevin Griffin <kevin@kevgriffin.com>
2025-03-06 20:39:50 -0500

    tweak

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  })
+  output: 'static'
 });
