0168b8e34048305ddbb8dedf172711f9618021c5
Kevin Griffin <kevin@kevgriffin.com>
2025-03-07 14:37:55 -0500

    SEO stuff (#11)
    * wip
    
    * tweak
    
    * tweak
    
    * tweak

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static'
+  output: 'static',
+  site: 'https://757tech.org',
+  integrations: [
+    sitemap({
+      // Configuration options
+      filter: (page) => !page.includes('/api/'), // Exclude API routes
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date(),
+    }),
+  ]
 });
